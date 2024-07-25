c9bfd912107d313343bf4bd7a5e124da0ce3890f
Ned <nedturner613@gmail.com>
2024-07-25 16:50:23 -0500

    Added simple interface

--- CsvGoodTimePlayer/Program.cs
+++ CsvGoodTimePlayer/Program.cs
@@ -16,8 +16,16 @@ class Program
 
         TipService tipper = new TipService(uri,client);
 
+        int order_id = 0;
+        
+        Console.Write("Enter Order Id: ");
+        if(int.TryParse(Console.ReadLine(),out int toUse))
+        {
+            order_id = toUse;
+        }
+
         // Get by Order Id
-        var theTip = await tipper.GetTipByOrderId(80);
+        var theTip = await tipper.GetTipByOrderId(order_id);
         if(theTip!= null)
         Console.WriteLine(theTip.Describe());
         else
